b0d623080c41634708c2eac287eb36010da69aba
Celso <celsobenedetti2@gmail.com>
2022-06-07 17:20:32 -0300

    fix: clean up nestjs starter boilerplate

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthController } from './auth/auth.controller';
 import { DbModule } from './db/db.module';
 
 @Module({
   imports: [DbModule],
-  controllers: [AppController, AuthController],
-  providers: [AppService],
+  controllers: [AuthController],
+  providers: [],
 })
 export class AppModule {}
