f968ff874bc2837ecc804e0fb6d8ace011c68ea6
Celso <celsobenedetti2@gmail.com>
2022-06-07 17:24:44 -0300

    feat: main.ts use .env port

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import 'dotenv/config';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+
+const PORT = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.use(helmet());
-  await app.listen(3000);
+  await app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
 }
 bootstrap();
