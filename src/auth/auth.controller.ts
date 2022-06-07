2b2d90a7210d35d083a7caadb17b04c8bd60e171
Celso <celsobenedetti2@gmail.com>
2022-06-07 17:02:34 -0300

    feat: auth controller setup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Post } from '@nestjs/common';
+import { DbService } from 'src/db/db.service';
 
 @Controller()
 export class AuthController {
+  constructor(private readonly dbService: DbService) {}
+
   @Post('/signin')
   signIn(): string {
+    this.dbService.query('SELECT * FROM users;', []);
     return 'signin in...';
   }
 
