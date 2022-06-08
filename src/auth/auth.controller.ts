6fc03eb0c96ddf2be6479cc12420cb1407b680a9
Celso <celsobenedetti2@gmail.com>
2022-06-08 15:41:50 -0300

    feat: implement signin/signup flows, auth validation and jwt issuing

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,22 +1,29 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
-import { SignInDto, SignUpDto } from './auth.dto';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { SignUpDto } from './auth.dto';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @HttpCode(200)
+  @UseGuards(LocalAuthGuard)
   @Post('/signin')
-  signIn(@Body() { email, password }: SignInDto) {
-    const result = this.authService.SignIn(email);
-    return result;
+  async signIn(@Request() req) {
+    return this.authService.signUserIn(req.user);
   }
 
   @HttpCode(201)
   @Post('/signup')
-  signUp(@Body() signUpDto: SignUpDto) {
-    const result = this.authService.SignUp();
-    return result;
+  async signUp(@Body() signUpDto: SignUpDto) {
+    await this.authService.signUserUp(signUpDto);
   }
 }
