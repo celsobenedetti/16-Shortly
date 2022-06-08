b64da8f386789ba8f94482bf3f16e6988e06fed4
Celso <celsobenedetti2@gmail.com>
2022-06-08 15:40:44 -0300

    feat: add UserModule and UserService

---
+++ src/user/user.repository.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { DbService } from 'src/common/db/db.service';
+import { UserInfo } from 'src/common/types';
+
+@Injectable()
+export class UserRepository {
+  constructor(private readonly db: DbService) {}
+
+  async findUserByEmail(email: string): Promise<UserInfo> {
+    const { rows } = await this.db.query(
+      `SELECT * FROM users WHERE email = $1;`,
+      [email],
+    );
+    return rows[0];
+  }
+
+  async insertUser(userInfo: UserInfo) {
+    const { name, email, password } = userInfo;
+    return this.db.query(
+      `INSERT INTO users (name, email, password) VALUES ($1, $2, $3);`,
+      [name, email, password],
+    );
+  }
+}
