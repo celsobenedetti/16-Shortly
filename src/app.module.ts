ebb77dbf455be496410b1dcdab39cb021fdd4b3e
Celso <celsobenedetti2@gmail.com>
2022-06-09 13:55:26 -0300

    chore: cleaned auth controller and typing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { DbModule } from './common/db/db.module';
+import { UrlsModule } from './urls/urls.module';
 
 @Module({
-  imports: [DbModule, AuthModule],
+  imports: [DbModule, AuthModule, UrlsModule],
 })
 export class AppModule {}
